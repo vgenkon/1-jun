488e9f23bc9d6e1b08d0ccaf33c63198e26bf799
Yevgeniy <vgenkon@gmail.com>
2022-06-02 01:51:24 +0600

    3

--- main.go
+++ main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	slice := []int{1, 2, 3, 4, 5, 5, 6, 7}
-	slice = append(slice[:4], slice[5:]...)
-	fmt.Println(slice)
+	array := [5]int{}
+	var a int
+	for i := 0; i < 5; i++ {
+		fmt.Scan(&a)
+		array[i] = a
+	}
+	var amax int
+	for _, value := range array {
+		if value > amax {
+			amax = value
+		}
+	}
+	fmt.Println(amax)
 }
