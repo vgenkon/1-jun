fe192e0c36c3dd8ad7cc3d5d86f46e2aaede3d87
Yevgeniy <vgenkon@gmail.com>
2022-06-05 02:28:32 +0600

    5 jun

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import "fmt"
 
+func evenSum(from, to int, ch chan int) {
+	result := 0
+	for i := from; i <= to; i++ {
+		if i%2 == 0 {
+			result += i
+		}
+	}
+	ch <- result
+}
+func squareSum(from, to int, ch chan int) {
+	result := 0
+	for i := from; i <= to; i++ {
+		if i%2 == 0 {
+			result += i * i
+		}
+	}
+	ch <- result
+}
 func main() {
-	var a, b float64
-	fmt.Scan(&a, &b)
-	fmt.Println((a + b) / 2)
+	evenCh := make(chan int)
+	sqCh := make(chan int)
+	go evenSum(0, 100, evenCh)
+	go squareSum(0, 100, sqCh)
+	fmt.Println(<-evenCh + <-sqCh)
 }
